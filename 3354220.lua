f7d0effc95b1a6a6291f18041e3a1879ffeb7d80
ksv <dasein470@gmail.com>
2025-11-28 23:32:16 -0500

    add new game 3354220

---
+++ 3354220.lua
@@ -0,0 +1,4 @@
+addappid(3354220) -- Comic Book Store Simulator
+addappid(3354221, 1, "ff11186d34fddf2d4dc2a6ef7affc6e8f231937087ed9cab96ca78436992a41f") -- Depot 3354221
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
