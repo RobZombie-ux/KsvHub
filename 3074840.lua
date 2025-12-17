fc405eba4adc60e9996284e4cd6ea1af16e4cdee
ksv <dasein470@gmail.com>
2025-12-16 21:11:28 -0500

    add new game 3074840

---
+++ 3074840.lua
@@ -0,0 +1,19 @@
+addappid(3074840)
+addappid(3074841, 1, "8f28dab410df56078a608788e3377f4c463ad81f28ebc78aa078ac1198d0991a")
+addappid(3959270) -- Utawarerumono ZAN - DLC Character Oshtor
+addappid(3959280) -- Utawarerumono ZAN - DLC Character Munechika
+addappid(3978880) -- Utawarerumono ZAN - DLC Outfit Haku
+addappid(3978900) -- Utawarerumono ZAN - DLC Outfit Kuon
+addappid(3978920) -- Utawarerumono ZAN - DLC Outfit Rulutieh
+addappid(3978930) -- Utawarerumono ZAN - DLC Outfit Nekone
+addappid(3978940) -- Utawarerumono ZAN - DLC Outfit Kiwru
+addappid(3978950) -- Utawarerumono ZAN - DLC Outfit Uruuru  Saraana
+addappid(3978960) -- Utawarerumono ZAN - DLC Outfit Nosuri
+addappid(3978970) -- Utawarerumono ZAN - DLC Outfit Ougi
+addappid(3978980) -- Utawarerumono ZAN - DLC Outfit Ukon
+addappid(3978990) -- Utawarerumono ZAN - DLC Outfit Maroro
+addappid(3979010) -- Utawarerumono ZAN - DLC Outfit Atuy
+addappid(3979020) -- Utawarerumono ZAN - DLC Outfit Jachdwalt
+addappid(4201540) -- Utawarerumono ZAN - DLC Outfit Kuons Hood
+addappid(4218120) -- Utawarerumono ZAN Sticker Set 1
+addappid(4218130) -- Utawarerumono ZAN Sticker Set 2
