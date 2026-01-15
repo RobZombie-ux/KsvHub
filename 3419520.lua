9c8148433d7d534d1385fc1f56e6b359dca38068
ksv <dasein470@gmail.com>
2026-01-15 12:25:59 -0500

    add new game 3419520

---
+++ 3419520.lua
@@ -0,0 +1,9 @@
+addappid(3419520)
+addappid(3419521, 1, "7763e89ed36372394d459530e45e7dab3aca85588166e1c59d3bd1f7993bfac6")
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8")
+addappid(3960620) -- Quarantine Zone Twitch Drop - Rat Skin - Raccoon
+addappid(3960630) -- Quarantine Zone Twitch Drop - Military Skin
+addappid(3960640) -- Quarantine Zone Twitch Drop - Scanner Skin
+addappid(3960650) -- Quarantine Zone Twitch Drop - Hammer Skin
+addappid(3960660) -- Quarantine Zone Twitch Drop - Thermopulse
