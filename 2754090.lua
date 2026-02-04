b4902c762282e54a2411e0988a4ae52364db7a01
ksv <dasein470@gmail.com>
2026-02-03 20:22:48 -0500

    add new game 2754090

---
+++ 2754090.lua
@@ -0,0 +1,7 @@
+addappid(2754090)
+addappid(2754091, 1, "e85bca1baec397ae81eb82e9f92147d42f0f361b72899b8b70cd2e2e16f3bbc5")
+addappid(2906020) -- Aces of Thunder - F4U-4
+addappid(3679010) -- Aces of Thunder - Hurricane Mk IV
+addappid(3679020) -- Aces of Thunder - SBD-3
+addappid(3679030) -- Aces of Thunder - P-40E-1
+addappid(3679040) -- Aces of Thunder - Spitfire Mk Ia
