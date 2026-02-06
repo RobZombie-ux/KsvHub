0deb5313a4a9872fea025cf3e13c048a09252b87
ksv <dasein470@gmail.com>
2026-02-06 09:46:48 -0500

    add new game 3681010

---
+++ 3681010.lua
@@ -0,0 +1,14 @@
+addappid(3681010, 1, "accea0016c6c5df7d031a08df7a460e90673b8da5c7cfb57432bb7e962893897")
+addappid(3681011, 1, "1c16eb875f0bd40521cc0ddb2ff4eb5772f0b1976252122abc5f9a09ddaaf995")
+addappid(3681013, 1, "b97a4048032dc27c3cf1c6887e6069b0f0d51da2c352c671278b00eb4423de35")
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358")
+addappid(4003880) -- Nioh 3 - Hellfire Equipment Set (Hellfire Warrior Armor set  Hellfire Ninja Attire set)
+addappid(4003890) -- Nioh 3 - Scampuss Furball Netsuke Charm
+addappid(4003900) -- Nioh 3 - Hellfrost Equipment Set (Hellfrost Warrior Armor set  Hellfrost Ninja Attire set)
+addappid(4003910) -- Nioh 3 - Infernal Weapons Set
+addappid(4003920) -- Nioh 3 - Kodama Netsuke Charm
+addappid(4003930) -- Nioh 3 Season Pass
+addappid(4003940) -- Nioh 3 - Chijiko Netsuke Charm
+addappid(4232000) -- Nioh 3 - Kusanagi Netsuke Charm
+addappid(4232020) -- Nioh 3 - Youngblood Armor
+addappid(4232030) -- Nioh 3 - Lone Wolfs Armor
