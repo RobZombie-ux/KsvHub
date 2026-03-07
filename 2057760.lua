cbb3d615fe061c84fa829c94f24eab367ec293ae
ksv <dasein470@gmail.com>
2026-03-07 18:34:15 -0500

    add new game 2057760

---
+++ 2057760.lua
@@ -0,0 +1,8 @@
+addappid(2057760) -- Esoteric Ebb
+-- MAIN APP DEPOTS
+addappid(2057761, 1, "5050ce648e520ef0449d3e690298b5f6142808a8660fca2bdcda4aeefe866544") -- Depot 2057761
+--setManifestid(2057761, "8261645904015930024", 7141047739)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Esoteric Ebb Digital Assets (AppID: 4438390) - missing depot keys
+-- addappid(4438390)
