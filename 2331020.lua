6b2ab5b074b2f293e52541fff760b59a3bac2059
ksv <dasein470@gmail.com>
2026-03-07 23:12:21 -0500

    add new game 2331020

---
+++ 2331020.lua
@@ -0,0 +1,30 @@
+addappid(2331020, 1, "ba3faa2ee3c8f345222ffd462c28e1487657e9044dd19677a6a62467b997b2dd") -- Tales of Berseria Remastered
+-- MAIN APP DEPOTS
+addappid(2331021, 1, "4c296709f48c48bc8fb38dde91a407523d6563d56af48d5b741f79e4c088b990") -- Depot 2331021
+--setManifestid(2331021, "198363463676703228", 13469616833)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+--setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITH DEDICATED DEPOTS
+-- Tales of Berseria Remastered - Digital Artbook  Soundtrack (AppID: 2928060)
+addappid(2928060)
+addappid(2928060, 1, "a155487ed7105b9c95c700d97886764d3724978fe369e84bffa0b5fb6c65b85e") -- Tales of Berseria Remastered - Digital Artbook  Soundtrack - Depot 2928060
+--setManifestid(2928060, "5547438104094267514", 1435347436)
+-- Tales of Berseria Remastered - Battle BGM Pack (AppID: 3157650)
+addappid(3157650)
+addappid(3157650, 1, "5ffac903e2bc90905dbead2653a94a129f37c64ccc8a0175f588649955ff0e82") -- Tales of Berseria Remastered - Battle BGM Pack - Depot 3157650
+setManifestid(3157650, "3704221288117614949", 101480700)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3157570) -- Tales of Berseria Remastered - Lv. Up5 (1)
+addappid(3157580) -- Tales of Berseria Remastered - Lv. Up5 (2)
+addappid(3157590) -- Tales of Berseria Remastered - Lv. Up5 (3)
+addappid(3157600) -- Tales of Berseria Remastered - Lv. Up5 (4)
+addappid(3157610) -- Tales of Berseria Remastered - Lv. Up10 (1)
+addappid(3157620) -- Tales of Berseria Remastered - Lv. Up10 (2)
+addappid(3157630) -- Tales of Berseria Remastered - Super Adventuring Assistance Set
+addappid(3157640) -- Tales of Berseria Remastered - Super Growth Support Herb Set
+addappid(3177880) -- Tales of Berseria Remastered - Deluxe Upgrade Pack
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2331022) -- Depot 2331022 (empty depot)
