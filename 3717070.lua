71e42e93a9a52413bd967ff90fd80389553ec9fb
ksv <dasein470@gmail.com>
2026-03-13 00:13:36 -0500

    add new game 3717070

---
+++ 3717070.lua
@@ -0,0 +1,13 @@
+addappid(3717070)
+addappid(3717071, 1, "1bf7a881a3e1aceec7d3d40622e631e3d7472b7fb4a051051a5e151bb63d7c21")
+--setManifestid(3717071, "4856188368828642086", 137661376532)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
+--setManifestid(228989, "3514306556860204959", 39590283)
+addappid(4125680) -- WWE 2K26 MyRISE Boost
+addappid(4181550) -- WWE 2K26 King of Kings Edition Pack
+addappid(4181560) -- WWE 2K26 Attitude Era Edition Pack
+addappid(4181570) -- WWE 2K26 Monday Night War Edition Pack
+addappid(4181590) -- WWE 2K26 Ringside Pass Premium Season 1
+addappid(4333560) -- WWE 2K26 Season Pass
+addappid(4357050) -- WWE 2K26 Superstar Mega-Boost
+addappid(4429680) -- WWE 2K26 Monday Night War Edition
