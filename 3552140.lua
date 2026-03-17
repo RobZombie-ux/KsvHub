c210f2d56c07ac57ebe5c2efe3f4f5ae3692d5dc
ksv <dasein470@gmail.com>
2026-03-17 09:25:55 -0500

    add new game 3552140

---
+++ 3552140.lua
@@ -0,0 +1,8 @@
+addappid(3552140)
+addappid(3552141, 1, "8527bf46fe5454e6c8956a042fb1aee9a9792d7f4c73ebe85ab6924dfcc1c0fe")
+--setManifestid(3552141, "597674519106065238", 1866360519)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+--setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
