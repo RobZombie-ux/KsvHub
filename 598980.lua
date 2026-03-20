8d50f7478930577d3b3b312b833fd123727b6928
ksv <dasein470@gmail.com>
2026-03-20 18:56:17 -0500

    add new game 598980

---
+++ 598980.lua
@@ -0,0 +1,7 @@
+addappid(598980, 1, "c21a454ef1de1cabb04e7a9c0b680cd5d91c256adf16dd90683d0851fbd409a6") -- The Coin Game
+addappid(598981, 1, "5eef890e869bba07fb135158a507ede6b276453d706d5c4e36f2ac5a66e43963") -- The Coin Game Content WINDOWS
+--setManifestid(598981, "7423561695571616238", 4784220634)
+addappid(598984, 1, "8cd74454dd76b1413f9e9edce77dc07524709e46e9cca1523699953c636ad7dd") -- The Coin Game Content WINDOWS 64
+--setManifestid(598984, "7575398112262625994", 6132992033)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1267250) -- The Coin Game - Supporter Edition
