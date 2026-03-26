63cd959b0efb51c93e1d28b7055fe0cd08569841
ksv <dasein470@gmail.com>
2026-03-25 22:28:07 -0500

    add new game 2815070

---
+++ 2815070.lua
@@ -0,0 +1,14 @@
+addappid(2815070) -- RIDE 6
+-- MAIN APP DEPOTS
+addappid(2815071, 1, "daec92978f1e0a7c2eac9986cc7e2754d247b951584e6178d0e97cc4d29fa7bb") -- Depot 2815071
+--setManifestid(2815071, "7691558688175097085", 58373621360)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+--setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3970220) -- RIDE 6 - Season Pass Vol. 1
+addappid(3970260) -- RIDE 6 - Credits Multiplier
+addappid(3970270) -- RIDE 6 - Welcome Bikes Pack
+addappid(3970280) -- RIDE 6 - 101 Starter Pack
