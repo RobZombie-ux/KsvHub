42b6a1b8cc8341562831d73a347b9012f61640a9
ksv <dasein470@gmail.com>
2026-03-27 19:49:57 -0500

    add new game 3500390

---
+++ 3500390.lua
@@ -0,0 +1,18 @@
+addappid(3500390) -- Mega Man Star Force Legacy Collection
+-- MAIN APP DEPOTS
+addappid(3500391, 1, "febf6d8948ae4c2e1a199fa36f58cbfd6c5b3d81c130a612604f803316f5326b") -- Depot 3500391
+--setManifestid(3500391, "4414609150574936853", 5693978069)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+--setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- Mega Man Star Force Legacy Collection  Sonia Strumm and Luna Platz Character Model Pack (AppID: 4059860)
+addappid(4059860)
+addappid(4059860, 1, "a4a1b191f1d28dd691f0e9197d0f156a0781b407a466d44a7e455aa97903b032") -- Mega Man Star Force Legacy Collection  Sonia Strumm and Luna Platz Character Model Pack - Depot 4059860
+--setManifestid(4059860, "1976583030948263159", 16410091)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Mega Man Star Force Legacy Collection  Geo Stelar and Omega-Xis Character Model Pack (AppID: 4059840) - missing depot keys
+-- addappid(4059840)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(4390970) -- Depot 4390970 (empty depot)
