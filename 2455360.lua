51cc0fbb2162c154d7c62000f213ab727ea06e5d
ksv <dasein470@gmail.com>
2026-03-31 21:26:52 -0500

    add new game 2455360

---
+++ 2455360.lua
@@ -0,0 +1,4 @@
+addappid(2455360) -- Cooking Simulator 2: Better Together
+-- MAIN APP DEPOTS
+addappid(2455361, 1, "246c514beec3f2192841f0ad37f41d13200f24a0b56edf2d21111c6a688e0a09") -- Depot 2455361
+--setManifestid(2455361, "51284401926510329", 10090875101)
