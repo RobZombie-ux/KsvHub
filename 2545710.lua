8889656488b3d6189f117540ee72ba294ea2974a
ksv <dasein470@gmail.com>
2026-04-01 19:16:07 -0500

    Update 5 files

---
+++ 2545710.lua
@@ -0,0 +1,15 @@
+addappid(2545710) -- Tony Hawk's™ Pro Skater™ 3 + 4
+-- MAIN APP DEPOTS
+addappid(2545711, 1, "cc91db4546139b89ff7216df6c489b4c157da8515a88f2491921ae845b9dab5a") -- Depot 2545711
+--setManifestid(2545711, "1813465191808461241", 151801820)
+addappid(2545712, 1, "8027343d1efbd17177d104a56462394d6507a9d68f348e96e365b9d9db954804") -- Depot 2545712
+--setManifestid(2545712, "4547333314972675313", 54939715744)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+--setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3315210) -- Tony Hawks Pro Skater 3  4 - Digital Deluxe Edition Content
+addappid(3529150) -- Tony Hawks Pro Skater 3  4 - Wireframe Tony Shader
+addappid(3529160) -- Tony Hawks Pro Skater 3  4 - Content Pack
