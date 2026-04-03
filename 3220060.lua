445665b34fe6af679ee24d84b7358dd2d87bec35
ksv <dasein470@gmail.com>
2026-04-03 17:28:21 -0500

    add new game 3220060

---
+++ 3220060.lua
@@ -0,0 +1,4 @@
+addappid(3220060) -- The Weeping Swan: Ten Days of the City's Fall
+-- MAIN APP DEPOTS
+addappid(3220061, 1, "63c6e0eb080e22155cdf3dfa2aef1c9f0f54c9ce06058a349237067e1f6756c4") -- Depot 3220061
+--setManifestid(3220061, "4031000303260723237", 5696445110)
