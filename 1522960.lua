2271975ee24deaf7ee260b480a2e513351e965f1
RobZombie-ux <dasein470@gmail.com>
2026-05-07 13:13:45 -0500

    add new game 1522960

---
+++ 1522960.lua
@@ -0,0 +1,4 @@
+addappid(1522960) -- Anemoiapolis
+-- MAIN APP DEPOTS
+addappid(1522961, 1, "4ae1b706c46cc1e3aca6e2040b858a5e0c2eab28033eabed5ac691e9f78ac1d3") -- Depot 1522961
+--setManifestid(1522961, "5159803858357428533", 2112910045)
