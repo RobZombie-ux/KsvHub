dfb9576e4a02fb2b34d5f977fa6be1be710c0748
RobZombie-ux <dasein470@gmail.com>
2026-05-12 22:07:59 -0500

    add new game 2483190

---
+++ 2483190.lua
@@ -0,0 +1,13 @@
+addappid(2483190) -- Forza Horizon 6
+addtoken(2483190, "2264898939686399139")
+-- MAIN APP DEPOTS
+addappid(2483191, 1, "f2a09f0a31b4df681ec0d2d572f368dfa4cd6c37ae67ebdfd591cfbe1334fcd9") -- Depot 2483191
+--setManifestid(2483191, "4760319112940000741", 167103384905)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+--setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4444140) -- Forza Horizon 6 TBDS2
+addappid(4444150) -- Forza Horizon 6 1962 Peel P50 Trolli Edition
