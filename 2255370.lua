c44e5c1feb41681a3319c3932a7b63d25478f4a0
RobZombie-ux <dasein470@gmail.com>
2026-05-12 20:26:30 -0500

    add new game 2255370

---
+++ 2255370.lua
@@ -0,0 +1,13 @@
+addappid(2255370) -- Directive 8020
+-- MAIN APP DEPOTS
+addappid(2255378, 1, "f60876d9515aa299bf429dc12729bc5cff3582e82dcc6846811997afbc752238") -- Depot 2255378
+--setManifestid(2255378, "1826872040144778522", 38193335334)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+--setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+--setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3883930) -- Directive 8020 - Digital Deluxe Upgrade
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2255379) -- Depot 2255379 (empty depot)
